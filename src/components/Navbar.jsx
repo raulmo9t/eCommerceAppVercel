1bf941a07d27bcceb471d047aa6f419a991ab1e5
raulmo9t <raulmo9t@gmail.com>
2025-06-21 12:47:52 +0530

    admin access added without auth

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,6 +45,7 @@ const Navbar = () => {
         </NavLink>
       </ul>
       <div className="flex items-center gap-6">
+        <a href={"https://e-commerce-app-admin-vercel.vercel.app/"}>Admin</a>
         <img
           onClick={() => setShowSearch(true)}
           src={assets.search_icon}
@@ -59,22 +60,22 @@ const Navbar = () => {
             alt=""
           />
           {/* Dropdown Menu */}
-          {token && (
-            <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
-              <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
-                <p className="cursor-pointer hover:text-black">My Profile</p>
-                <p
-                  onClick={() => navigate("/orders")}
-                  className="cursor-pointer hover:text-black"
-                >
-                  Orders
-                </p>
-                <p onClick={logout} className="cursor-pointer hover:text-black">
-                  Logout
-                </p>
-              </div>
+          {/* {token && ( */}
+          <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
+              <p className="cursor-pointer hover:text-black">My Profile</p>
+              <p
+                onClick={() => navigate("/orders")}
+                className="cursor-pointer hover:text-black"
+              >
+                Orders
+              </p>
+              <p onClick={logout} className="cursor-pointer hover:text-black">
+                Logout
+              </p>
             </div>
-          )}
+          </div>
+          {/* )} */}
         </div>
         <Link to="/cart" className="relative">
           <img src={assets.cart_icon} className="w-5 min-w-5" alt="" />
